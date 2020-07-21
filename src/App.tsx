47159874fab7c6309881859edea6dcdeafcd0f19
托尼 <yinchao.zhou@hipac.cn>
2020-07-21 20:09:27 +0800

    feat: 脚手架升级

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: 托尼
+ * @Date: 2020-07-21 20:02:38
+ * @LastEditors: 托尼
+ * @LastEditTime: 2020-07-21 20:02:52
+ */
+
+import React from 'react'
+import './index.less'
+
+const App = () => {
+  return (
+    <div className="main">App</div>
+  )
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
-/**
- * Created by tony on 2019-12-16
- */
+/*
+ * @Author: 托尼
+ * @Date: 2020-04-18 01:37:51
+ * @LastEditors: 托尼
+ * @LastEditTime: 2020-07-21 20:08:18
+ */ 
+
+import 'core-js/stable'
+import '@babel/runtime/regenerator'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Example from './Example'
+import App from './App'
 
-ReactDOM.render(<Example />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
 
 if (module.hot) {
   module.hot.accept()
